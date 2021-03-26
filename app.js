9ce54dd77092642efa96b30840245c1882a4bc33
Jay Chheda <jaychheda97@gmail.com>
2021-03-26 12:24:14 +0530

    add function to evaluate and calculate and clear display

--- app.js
+++ app.js
@@ -6,10 +6,76 @@ const equalButton = document.querySelector('[data-equal]');
 const dotButton = document.querySelector('[data-dot]');
 const clearButton = document.querySelector('[data-clear]');
 const allClearButton = document.querySelector('[data-ac]');
-let firstOperand = null;
-let secondOperand = null;
+let firstOperand = '';
+let secondOperand = '';
 let currentOperation = null;
-let resetScreen = false;
+let shouldResetScreen = false;
+
+equalButton.addEventListener('click', operate);
+
+allClearButton.addEventListener('click',allClear);
+clearButton.addEventListener('click', deleteOne);
+
+dotButton.addEventListener('click',appendDot);
+
+numberButtons.forEach(button => {
+          button.addEventListener('click',()=>{
+  appendNumber(button.textContent);
+    });
+    
+});
+
+operatorButtons.forEach(button=>button.addEventListener('click',(ev)=>{
+    setOperation(button.textContent);
+}));
+
+
+function setOperation(operator){
+    if(currentOperation!==null) operate();
+    currentOperation = operator;
+    firstOperand = displayScreen.textContent;
+    shouldResetScreen = true;
+
+    
+}
+
+function operate(){
+    if(currentOperation ==null || shouldResetScreen) return;
+    if(displayScreen.textContent ==='0' && currentOperation =='/'){
+        alert('cannot divide by 0');
+        allClear();
+        return;
+    }
+    secondOperand = displayScreen.textContent;
+    displayScreen.textContent = (Math.round(evaluate(currentOperation,firstOperand,secondOperand)*1000)/1000);
+    currentOperation = null;
+
+
+
+}
+
+
+
+function appendNumber(number){
+    if(shouldResetScreen || displayScreen.textContent ==='0') resetDisplay();
+    displayScreen.textContent += number;
+
+}
+
+function appendDot(){
+    if(shouldResetScreen) resetDisplay();
+    if(displayScreen.textContent =='') displayScreen.textContent ='0';
+    if(displayScreen.textContent.includes('.')) return;
+    displayScreen.textContent+= '.';
+
+}
+
+
+
+
+
+
+
 
 
 
@@ -67,5 +133,20 @@ function divide(number1,number2){
 
 function resetDisplay(){
     displayScreen.innerText = '';
-    resetScreen =false;
+    shouldResetScreen =false;
+}
+
+function allClear(){
+    displayScreen.innerText = '0';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
+allClear();
+
+function deleteOne(){
+    displayScreen.textContent = displayScreen.textContent.toString().slice(0,-1);
+    if(displayScreen.textContent ===''){
+        displayScreen.textContent = '0';
+    }
 }
